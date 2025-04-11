7123ba07767d150267d3a4042297aca786e70644
Andree✨ <153280463+andreeNewbie@users.noreply.github.com>
2025-04-11 22:23:53 +0700

    Resolved conflict

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,19 +6,14 @@ public class UIController : MonoBehaviour
 {
     public static UIController Instance;
 
-<<<<<<< HEAD
     public GameObject pausePanel;
 
-    [SerializeField] private Slider energySlider;
-    [SerializeField] private TMP_Text energyText; 
-=======
     [SerializeField] private Slider energySlider;
     [SerializeField] private TMP_Text energyText; 
 
     [SerializeField] private Slider healthSlider;
     [SerializeField] private TMP_Text healthText; 
 
->>>>>>> origin/main
     private void Awake(){
         if (Instance != null){
             Destroy(gameObject);
@@ -26,12 +21,6 @@ public class UIController : MonoBehaviour
             Instance = this;
         }
     }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/main
-
     
     public void UpdateEnergySlider(float current, float max){
         energySlider.value = Mathf.RoundToInt(current); // Round the current energy value to the nearest integer
