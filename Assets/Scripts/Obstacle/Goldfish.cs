ecaea713059fef21c141d2f3da5c08e4b30516b6
HuyenHuyenUwU <itsmenamhuyen@gmail.com>
2025-05-04 05:42:06 +0700

    Tinh chỉnh lại goldfish

--- Assets/Scripts/Obstacle/Goldfish.cs
+++ Assets/Scripts/Obstacle/Goldfish.cs
@@ -3,49 +3,22 @@ public class GoldFish : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
 
-    private Vector3 basePos; // Vị trí cơ bản khi ko có floating
-    private Vector3 movementDirection = Vector3.zero; // Hướng di chuyển của cá vàng
-
-    [SerializeField] private float moveSpeed  = 0.5f;
-    [SerializeField] public float directionChangeInterval = 1.5f; // Thời gian giữa các lần đổi hướng
-    private float timeToNextDirectionChange;
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        basePos = GeneratePositionWithGA();
-        transform.position = basePos;
-
-        PickNewDirection(); // Chọn hướng di chuyển ngẫu nhiên ngay từ đầu
-        timeToNextDirectionChange = directionChangeInterval;
+        transform.position = GeneratePositionWithGA(); 
     }
 
     private void Update()
-    {
-        // Cách vài giây đổi hướng
-        timeToNextDirectionChange -= Time.deltaTime;
-        if (timeToNextDirectionChange <= 0)
-        {
-            PickNewDirection();
-            timeToNextDirectionChange = directionChangeInterval;
-        }
-        // di chuyển theo hướng đã chọn
-        basePos += movementDirection * moveSpeed * Time.deltaTime;
-        
+    {        
         // floating effect
-        float move_y = Mathf.Sin(Time.time) * 0.3f * Time.deltaTime * GameManager.Instance.worldSpeed;
+        float move_y = Mathf.Sin(Time.time) * 0.3f * Time.deltaTime * GameManager.Instance.worldSpeed;        
         float move_x = GameManager.Instance.worldSpeed * Time.deltaTime;
-        
-        // Vị trí cuối cùng là vị trí cơ bản + hiệu ứng lên xuống
-        // transform.position = new Vector3(
-        //     basePos.x - move_x,
-        //     basePos.y + move_y,
-        //     basePos.z
-        // );
 
         transform.position += new Vector3(-move_x, move_y); // Di chuyển đối tượng về phía trước
 
         if (Mathf.Abs(transform.position.x) > 11f){
-            Destroy(gameObject); // Destroy the asteroid if it goes out of bounds
+            Destroy(gameObject); 
         }
     }
 
@@ -56,11 +29,6 @@ public class GoldFish : MonoBehaviour
             GameManager.Instance.goldfishCounter++;
             Destroy(gameObject); 
         }
-    }
-    private void PickNewDirection()
-    {
-        float angle = Random.Range(0, 360) * Mathf.Deg2Rad;
-        movementDirection = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0).normalized;
     }
     private Vector3 GeneratePositionWithGA()
     {
